33b021f41d3d9249598e0dc37fc34135d8994148
Polytopamine <125090675+Polytopamine@users.noreply.github.com>
2023-03-05 22:17:27 +0000

    commit 2

--- quadratic FEM main.py
+++ quadratic FEM main.py
@@ -28,12 +28,7 @@ class Element:
         if len(nodes) == 2 : self.k = k_linear
         if len(nodes) == 3 : self.k = k_quadratic
 
-
-        
-
         elem_list.append(self)
-
-
 
 
 
@@ -48,18 +43,13 @@ class Node:
 
 
 
-
-
 import numpy as np
 from operator import attrgetter
 #from FEM_classes_definitions import Node , Element , node_list , elem_list
 
 
-
-
-
 #-----------------------------------------------------------
-
+'''
 E = 200000
 A = 100000
 L = 5000
@@ -67,6 +57,7 @@ F = 100
 
 
 #        ID , force, displacement --> None if unknown
+
 N0 = Node(0 , None, 0)
 N1 = Node(1 , 0, None)
 N2 = Node(2 , F,    None)
@@ -77,18 +68,61 @@ N4 = Node(4 , 2*F,  5)
 #           ID , nodes(list) , A , E , L
 E0 = Element(0 , [N0 , N1 , N2], A , E , L )
 E1 = Element(1 , [N2 , N3 , N4], 2*A , E , L )
+'''
 
 
-# E1 = LinearElement(1 , N1 , N2 , 2*A , 2*E , L )
-# E2 = LinearElement(2 , N2 , N3 , A , 2*E , L )
-# E3 = LinearElement(3 , N3 , N4 , 2*A , E , L )
-# E4 = LinearElement(4 , N4 , N5 , A , 2*E , 2*L )
-# E5 = LinearElement(5 , N5 , N6 , 2*A , 2*E , 2*L )
+
+# 5.3--worked example 10
+E = 200000
+A = 1000000
+L = 1000
+F = 100
+#        ID, F, D
+N0 = Node(0, None, 0)
+N1 = Node(1, 250, None)
+N2 = Node(2, 100, None)
+#           ID , nodes(list) , A , E , L
+E0 = Element(0 , [N0, N1, N2], 0.1*A , E , 5*L )
+#resuts as expected
+
+'''
+#Tutuorial quesiton 1.10
+E = 200000
+A = 1000000
+L = 1000
+F = 100
+#         ID, F, D
+N0 = Node(0, None, 0)
+N1 = Node(1, 3*F)
+N2 = Node(2, 2*F)
+N3 = Node(3, 1*F)
+#            ID , nodes(list) , A , E , L
+E0 = Element(0 , [N0, N1], 0.9*A , E , L )
+E1 = Element(1 , [N1, N2], 0.6*A , E , L )
+E2 = Element(2 , [N2, N3], 0.3*A , E , L )
+#results correct
+'''
+'''
+#tutorial quesiton 2.3
+E = 200000
+A = 1000000
+L = 10000
+
+#         ID, F, D
+N0 = Node(0, None, 0)
+N1 = Node(1, 350)
+N2 = Node(2, 100)
+
+#            ID , nodes(list) , A , E , L
+E0 = Element(0 , [N0, N1, N2], 0.4*A , E , L )
+#results correct
+'''
 
 
 
 
 #-----------------------------------------------------------
+
 
 #find smallest stiffnessfactor of the elememts
 
@@ -112,29 +146,28 @@ for elem in elem_list:
             K[elem.nodes[i].ID, elem.nodes[j].ID ] += elem.k[i,j]
 
 
-print('K :')
-print(K)
+print(f'K :\n{K}')
 
 
 #create stiffness matrix that takes into account the properties of the elements
 K = K*min_sf
-print("\nK*EA/L :")
-print(K)
+print(f"\nK*EA/L :\n{K}")
+
 
 #create the displacement matrix
 D = np.zeros((len(node_list),1))
 for i in node_list:
     D[i.ID,0] = i.displacement
-print('\nD :')
-print(D)
+print(f'\nD :\n{D}')
+
 
 
 #create the forces matrix
 F = np.zeros((len(node_list),1))
 for i in node_list:
     F[i.ID,0] = i.force
-print('\nF :')
-print(F)
+print(f'\nF :\n{F}')
+
 
 
 
@@ -155,32 +188,20 @@ for i in range(len(F)):
 
 
 F_m = np.delete(F, unwn_force_locs, 0)
-# print('F_m')
-# print(F_m)
 K_m = np.delete(np.delete(K, unwn_force_locs, 1), unwn_force_locs, 0)
-# print('K_m')
-# print(K_m)
-
 
 # do matric operation to find the displacements
 D_m = np.linalg.solve(K_m, F_m)
-# print('D_m:')
-# print(D_m)
 
 
 # with the displacements found, find the forces by usign another matrix operation
-
 #Place the newfound displacements into the displacement matrix
-
 new_disp_locs = np.delete(np.arange(len(F)), unwn_force_locs,0) # displacement was calculated from all known forces, so diplacement at unknown forces have not been found yet
 # print(np.arange(len(F)),unwn_force_locs, new_disp_locs )
 for i in range(len(new_disp_locs)):
     D[new_disp_locs[i]] = D_m[i]
 
-print('\nnew D:')
-print(D)
-#new D seems correct accroding to worked example 7 lecture 4
-
+print(f'\nnew D:\n{D}')
 
 
 
@@ -188,31 +209,31 @@ print(D)
 
 #find the remaining unknown forces from the displacements
 
-#determine the locations of unknown displacements
-unwn_disp_locs = []
-for i in range(len(D)):
-    if np.isnan(D[i]) == True:
-        unwn_disp_locs.append(i)
-# print(unwn_disp_locs)
+# #determine the locations of unknown displacements
+# unwn_disp_locs = []
+# for i in range(len(D)):
+#     if np.isnan(D[i]) == True:
+#         unwn_disp_locs.append(i)
+# # print(unwn_disp_locs)
 
-#create new d and f matrixes without the unknow displacement locations
-D_m = np.delete(D, unwn_disp_locs, 0)
-# print('D_m')
-# print(D_m)
-K_m = np.delete(np.delete(K, unwn_disp_locs, 1), unwn_disp_locs, 0)
-# print('K_m')
-# print(K_m)
+# #create new d and f matrixes without the unknow displacement locations
+# D_m = np.delete(D, unwn_disp_locs, 0)
+# K_m = np.delete(np.delete(K, unwn_disp_locs, 1), unwn_disp_locs, 0)
+
 
 #calculate the forces from the displacements
-F = np.dot(K_m, D_m)
-print('\nnew F:')
-print(F)
-#seems correct, thought the rounding on the 0 is very slightly off
+F = np.dot(K, D)
+print(f'\nnew F:\n{F}')
+
 
 #updates the displacement and force values on the nodes
 for i in node_list:
     i.displacement = D[i.ID]
     i.force = F[i.ID]
+
+
+
+
 
 
 #Display the force and dissplacement at each node
@@ -232,12 +253,17 @@ for i in elem_list:
     # print(f'Elem {i.ID} :')
 
     i.strain = np.dot(np.array([-1/i.L , 1/i.L]),np.array([i.nodes[0].displacement[0], i.nodes[len(i.nodes)-1].displacement[0]]))
-    # print(np.array([-1/i.L , 1/i.L]))
-    # print(np.array([i.nodes[0].displacement[0], i.nodes[len(i.nodes)-1].displacement[0]]))
-    # print(np.dot(np.array([-1/i.L , 1/i.L]),np.array([i.nodes[0].displacement[0], i.nodes[2].displacement[0]])))
     print(f'\nElem {i.ID} : Strain = {i.strain:e}')
 
     i.stress = i.E * i.strain
     print(f'Elem {i.ID} : Stress = {i.stress:e} MPa')
 
     
+
+
+
+#still problem: 
+# -- if both displacement and force is known, 
+# the force sill be overwritten by the calculation from the displacement
+# -- the element stress and strain are not yet checked to work properly for quadratic (works good for linear)
+#would be cool to get the displacement along the length of the quad elements
