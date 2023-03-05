339c3f3904dc18258fdaaa9901fa164baa82cee2
Polytopamine <125090675+Polytopamine@users.noreply.github.com>
2023-03-05 22:14:35 +0000

    stress/strain at nodes/elements, linear and quad  elements automatically

--- quadratic FEM main.py
+++ quadratic FEM main.py
@@ -3,37 +3,33 @@ attempt to do the FEM optional coursework
 goal is to make a 1D FEM solver that uses quadratic elemtents and can any number of displacement nodes
 '''
 
-
-
-#define the system
-#stifness matrix
-#displacement metrix
-
-
-
-
-
 #FEM classes definitions
 
 import numpy as np
 
 
-
-
 elem_list = []
 node_list = []
 
-class QuadraticElement:
+k_quadratic = np.array([[7/3, -8/3, 1/3],[-8/3, 16/3, -8/3],[1/3, -8/3, 7/3]])
+k_linear = np.array([[1,-1],[-1,1]])
+
+class Element:
     """gives the attributes of the element"""
     def __init__(self , ID , nodes , A , E , L):
         self.ID = ID
-        self.nodes = nodes # should be a list of the different elements
+        self.nodes = nodes # should be a list of the different nodes the element is conmected to
 
         self.A = A 
         self.E = E 
         self.L = L 
         self.sf = (E*A)/L 
-        self.k = np.array([[7/3, -8/3, 1/3],[-8/3, 16/3, -8/3],[1/3, -8/3, 7/3]])
+
+        if len(nodes) == 2 : self.k = k_linear
+        if len(nodes) == 3 : self.k = k_quadratic
+
+
+        
 
         elem_list.append(self)
 
@@ -65,21 +61,22 @@ from operator import attrgetter
 #-----------------------------------------------------------
 
 E = 200000
-A = 10000
+A = 100000
 L = 5000
 F = 100
+
 
 #        ID , force, displacement --> None if unknown
 N0 = Node(0 , None, 0)
 N1 = Node(1 , 0, None)
 N2 = Node(2 , F,    None)
-# N3 = Node(3 , None, 1)
-# N4 = Node(4 , 2*F,  5)
+N3 = Node(3 , None, 1)
+N4 = Node(4 , 2*F,  5)
 
 
 #           ID , nodes(list) , A , E , L
-E0 = QuadraticElement(0 , [N0 , N1 , N2], A , E , L )
-# E1 = QuadraticElement(0 , [N2 , N3 , N4], 2*A , E , L )
+E0 = Element(0 , [N0 , N1 , N2], A , E , L )
+E1 = Element(1 , [N2 , N3 , N4], 2*A , E , L )
 
 
 # E1 = LinearElement(1 , N1 , N2 , 2*A , 2*E , L )
@@ -87,11 +84,6 @@ E0 = QuadraticElement(0 , [N0 , N1 , N2], A , E , L )
 # E3 = LinearElement(3 , N3 , N4 , 2*A , E , L )
 # E4 = LinearElement(4 , N4 , N5 , A , 2*E , 2*L )
 # E5 = LinearElement(5 , N5 , N6 , 2*A , 2*E , 2*L )
-
-
-
-
-
 
 
 
@@ -106,8 +98,7 @@ min_sf = min(elem_list , key=attrgetter('sf')).sf
 for i in elem_list:
     i.k = i.k * ( i.sf / min_sf )
 
-
-    #create the global K matrix using the multiples
+    #create the global K matrix using the multiples of the smallest value
 
 
 #create base stiffness matrix from elements
@@ -121,7 +112,6 @@ for elem in elem_list:
             K[elem.nodes[i].ID, elem.nodes[j].ID ] += elem.k[i,j]
 
 
-
 print('K :')
 print(K)
 
@@ -131,40 +121,20 @@ K = K*min_sf
 print("\nK*EA/L :")
 print(K)
 
-
-#create the forces matrix
-
-F = np.zeros((len(node_list),1))
-
-for i in node_list:
-    F[i.ID,0] = i.force
-
-print('\nF :')
-print(F)
-
-
 #create the displacement matrix
-
 D = np.zeros((len(node_list),1))
-
 for i in node_list:
     D[i.ID,0] = i.displacement
-
 print('\nD :')
 print(D)
 
 
-
- 
-
-
-#determine the locations of unknown forces
-unwn_force_locs = []
-for i in range(len(F)):
-    if np.isnan(F[i]) == True:
-        unwn_force_locs.append(i)
-# print(unwn_force_locs)
-
+#create the forces matrix
+F = np.zeros((len(node_list),1))
+for i in node_list:
+    F[i.ID,0] = i.force
+print('\nF :')
+print(F)
 
 
 
@@ -176,22 +146,26 @@ for i in range(len(F)):
 # by deleting them instead of constructing anoother without them
 # solving for displacement, so removing lines where the force is unknown because that would be unsolvable
 
+#determine the locations of unknown forces
+unwn_force_locs = []
+for i in range(len(F)):
+    if np.isnan(F[i]) == True:
+        unwn_force_locs.append(i)
+# print(unwn_force_locs)
+
+
 F_m = np.delete(F, unwn_force_locs, 0)
-print('F_m')
-print(F_m)
+# print('F_m')
+# print(F_m)
 K_m = np.delete(np.delete(K, unwn_force_locs, 1), unwn_force_locs, 0)
-print('K_m')
-print(K_m)
-
-
+# print('K_m')
+# print(K_m)
 
 
 # do matric operation to find the displacements
-
-
 D_m = np.linalg.solve(K_m, F_m)
-print('D_m:')
-print(D_m)
+# print('D_m:')
+# print(D_m)
 
 
 # with the displacements found, find the forces by usign another matrix operation
@@ -203,68 +177,67 @@ new_disp_locs = np.delete(np.arange(len(F)), unwn_force_locs,0) # displacement w
 for i in range(len(new_disp_locs)):
     D[new_disp_locs[i]] = D_m[i]
 
-print('new D:')
+print('\nnew D:')
 print(D)
 #new D seems correct accroding to worked example 7 lecture 4
+
+
+
 
 
 
 #find the remaining unknown forces from the displacements
 
 #determine the locations of unknown displacements
-
 unwn_disp_locs = []
 for i in range(len(D)):
     if np.isnan(D[i]) == True:
         unwn_disp_locs.append(i)
 # print(unwn_disp_locs)
 
+#create new d and f matrixes without the unknow displacement locations
 D_m = np.delete(D, unwn_disp_locs, 0)
-print('D_m')
-print(D_m)
+# print('D_m')
+# print(D_m)
 K_m = np.delete(np.delete(K, unwn_disp_locs, 1), unwn_disp_locs, 0)
-print('K_m')
-print(K_m)
+# print('K_m')
+# print(K_m)
 
-F = np.linalg.solve(K_m, D_m)
-print('new F:')
+#calculate the forces from the displacements
+F = np.dot(K_m, D_m)
+print('\nnew F:')
 print(F)
+#seems correct, thought the rounding on the 0 is very slightly off
+
+#updates the displacement and force values on the nodes
+for i in node_list:
+    i.displacement = D[i.ID]
+    i.force = F[i.ID]
 
 
+#Display the force and dissplacement at each node
+print('\nDissplacement at nodes:')
+for i in node_list:
+    print(f'Node {i.ID} = {round(i.displacement[0],14):e} mm')
+
+print('\nForces at nodes :')
+for i in node_list:
+    print(f'Node {i.ID} = {round(i.force[0],14):e} mm') #round the value to the 14th digit 
+    #then turns into scientific notation to elimintae python rounding errors
 
 
+#calculate the stress and strain in each elemenent based on the displacement at the nodes
+print('\nStress and strain in each element :')
+for i in elem_list:
+    # print(f'Elem {i.ID} :')
 
+    i.strain = np.dot(np.array([-1/i.L , 1/i.L]),np.array([i.nodes[0].displacement[0], i.nodes[len(i.nodes)-1].displacement[0]]))
+    # print(np.array([-1/i.L , 1/i.L]))
+    # print(np.array([i.nodes[0].displacement[0], i.nodes[len(i.nodes)-1].displacement[0]]))
+    # print(np.dot(np.array([-1/i.L , 1/i.L]),np.array([i.nodes[0].displacement[0], i.nodes[2].displacement[0]])))
+    print(f'\nElem {i.ID} : Strain = {i.strain:e}')
 
+    i.stress = i.E * i.strain
+    print(f'Elem {i.ID} : Stress = {i.stress:e} MPa')
 
-
-
-
-
-
-####testing stuff:
-
-# print('\nK:')
-# print(K)
-# # A = K * [[True],[True],[False]]
-# # A = K [True,True,False]
-# # print(A)
-# # print(A[0,0])
-# print('\n')
-# A = np.delete(K,1,0)
-# print(A)
-# print('\n')
-# A = np.delete(K,[0,1],0)
-# print(A)
-
-# A = np.arange(25).reshape(5,5)
-# print(A)
-# print('\n')
-# A = np.delete(A,[1,1,1,1,1],0)
-# print(A)
-
-# a = [1,0,0,1,0]
-# b=[]
-# for i in range(len(a)): 
-#     if a[i] ==1 :
-#         b.append(i)
-# print(b)
+    
